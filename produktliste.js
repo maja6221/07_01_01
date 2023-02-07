17dbe195b0fcd62914d01526e454a988c3d97111
Maja Goltermann <maja.goltermann@gmail.com>
2023-02-07 13:44:40 +0100

    kategorier

--- produktliste.js
+++ produktliste.js
@@ -1,4 +1,6 @@
-const url = `https://kea-alt-del.dk/t7/api/products?limit=50`;
+const urlParams = new URLSearchParams(window.location.search);
+const cat = urlParams.get("cat");
+const url = `https://kea-alt-del.dk/t7/api/products?limit=100&category=${cat}`;
 
 
 function hentData() {
@@ -11,6 +13,9 @@ const skabelon = document.querySelector("#minSkabelon").content;
 
 const beholder = document.querySelector("#main_produktliste");
 
+
+document.querySelector("h2").textContent = cat
+
 function visData(json) {
     console.log(json);
   json.forEach(produkt => {
@@ -19,7 +24,10 @@ function visData(json) {
     klon.querySelector(".articletype").textContent = produkt.articletype;
     klon.querySelector(".brand").textContent = produkt.brandname;
     klon.querySelector(".pris").textContent = produkt.price;
-    klon.querySelector(".procent").textContent = produkt.discount;
+    klon.querySelector(".procent").textContent = produkt.discount; 
+
+    //udsalgsprisen
+    klon.querySelector(".nypris span").textContent = produkt.price - (produkt.price / 100) * produkt.discount;
     
     klon.querySelector("a").href = "produkt.html?id=" + produkt.id;
 
